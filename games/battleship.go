63056b634918661de99b9b195d189b75c7cb673c
Taavi Kivisik <taavi.kivisik@gmail.com>
2018-09-26 23:01:34 +0200

    Minimally working Battleship

--- games/battleship.go
+++ games/battleship.go
@@ -1,5 +1,186 @@
 package games
 
+import (
+	"crypto/rand"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// boardSideLength determines the height and width of the game board
+// Letters hold the alphabet
+// maxShips determiens the number of ships per player
+// MaxShots determines the maximum length of a game
+const (
+	boardSideLength uint8  = 4
+	letters         string = "abcdefghijklmnopqrstuvwxyz"
+	maxShips        int    = 2
+	maxShots        int    = 100
+)
+
+// legend is a dictionary of program logic to map elements
+var legend = map[string]string{
+	"terrain": "~",
+	"ship":    "0",
+	"hit":     "X",
+	"miss":    "*",
+}
+
+// printLegend prints the legend on screen
+func printLegend() {
+	fmt.Println("")
+	for k, v := range legend {
+		fmt.Printf("%2s - %s    ", v, k)
+	}
+	fmt.Println("\n Valid point example: 'd 3'")
+	fmt.Print("\n")
+}
+
+// printBoard prints the current battleship board
+func printBoard(ships, shots map[int8]byte, enemy bool) int {
+	hitCount := 0
+
+	fmt.Print("\n  ")
+	for column := uint8(0); column < boardSideLength; column++ {
+		fmt.Printf("%2s", letters[column:column+1])
+	}
+	fmt.Println()
+
+	for row := uint8(0); row < boardSideLength; row++ {
+		fmt.Printf("%2d", row+1)
+		for column := uint8(0); column < boardSideLength; column++ {
+			if ships[int8(row)]&(1<<column) != 0 { // ship found
+				if shots[int8(row)]&(1<<column) != 0 { // shot found
+					fmt.Printf(" %s", legend["hit"])
+					hitCount++
+				} else {
+					if enemy { // hide enemy ships until hit
+						fmt.Printf(" %s", legend["terrain"])
+					} else {
+						fmt.Printf(" %s", legend["ship"])
+					}
+				}
+			} else { // not a ship
+				if shots[int8(row)]&(1<<column) != 0 { // shot found
+					fmt.Printf(" %s", legend["miss"])
+				} else {
+					fmt.Printf(" %s", legend["terrain"])
+				}
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+	return hitCount
+}
+
+// pointToMap updates the shots map
+func pointToMap(p point, landscape map[int8]byte) {
+	p.number--
+	var letterIndex = uint8(strings.Index(letters, p.letter))
+	landscape[int8(p.number)] |= 1 << uint8(letterIndex)
+}
+
+// Point expresses a location on a map as a
+type point struct {
+	letter string
+	number int
+}
+
+// PlaceShips guides the player to place its ships
+func placeShips(random bool) map[int8]byte {
+	var p point
+	ships := map[int8]byte{0: uint8(0)}
+	shots := map[int8]byte{}
+
+	if random {
+		for nShipsPlaced := 0; nShipsPlaced < maxShips; nShipsPlaced++ {
+			b := make([]byte, 2)
+			_, err := rand.Read(b)
+
+			if err != nil {
+				fmt.Println("error:", err)
+			}
+			p.letter = letters[int(b[0]%boardSideLength) : int(b[0]%boardSideLength)+1]
+			p.number = int(b[1] % boardSideLength)
+			p.number++ // Convert into a number from 1 to boardSideLength
+
+			//fmt.Printf("%d, %d", b[0]%boardSideLength, b[1]%boardSideLength)
+			pointToMap(p, ships)
+		}
+
+	} else {
+		fmt.Printf("Please choose the location of your %d ships (e.g. 'd 3')\n", maxShips)
+		for nShipsPlaced := 0; nShipsPlaced < maxShips; nShipsPlaced++ {
+			fmt.Scanln(&p.letter, &p.number)
+			pointToMap(p, ships)
+			printBoard(ships, shots, false)
+		}
+	}
+	return ships
+}
+
+func placeShot(ships, shots map[int8]byte, automatic bool) {
+	var p point
+
+	if automatic {
+		b := make([]byte, 2)
+		_, err := rand.Read(b)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+
+		p.letter = letters[int(b[0]%boardSideLength) : int(b[0]%boardSideLength)+1]
+		p.number = int(b[1] % boardSideLength)
+		p.number++ // Convert into a number from 1 to boardSideLength
+
+		pointToMap(p, shots)
+	} else {
+		fmt.Print("Please choose where to shoot (e.g. 'd 2')\n\n")
+		fmt.Scanln(&p.letter, &p.number)
+		pointToMap(p, shots)
+		printLegend()
+	}
+}
+
 // Battleship is a game to guess the location of ships
 func Battleship() {
+	fmt.Println("***********************")
+	fmt.Println("* BATTLESHIP THE GAME *")
+	fmt.Print("***********************\n\n")
+
+	var enemyScore, myScore int
+	enemyShips := placeShips(true)
+	enemyShots := map[int8]byte{0: uint8(0)}
+	myShips := placeShips(false)
+	myShots := map[int8]byte{0: uint8(0)}
+
+	for round := int8(0); int(round) < maxShots; round++ {
+		fmt.Println("ENEMY MOVE:")
+		fmt.Println("MY BOARD, ENEMY SHOTS:")
+		placeShot(myShips, enemyShots, true)
+		enemyScore = printBoard(myShips, enemyShots, false)
+
+		fmt.Println("\t\t\tMY MOVE:")
+		fmt.Println("\t\t\tENEMY BOARD, MY SHOTS:")
+		placeShot(enemyShips, myShots, false)
+		myScore = printBoard(enemyShips, myShots, true)
+
+		if enemyScore >= maxShips {
+			if myScore >= maxShips {
+				fmt.Println("IT'S A DRAW, GAME OVER, WELL DONE")
+			} else {
+				fmt.Println("GAME OVER, YOU LOST")
+			}
+			os.Exit(0)
+		} else {
+			if myScore >= maxShips {
+				fmt.Println("GAME OVER. YOU WON !!!")
+				os.Exit(0)
+			}
+		}
+
+		fmt.Println("===== ===== ===== ===== ===== ===== ===== ===== ===== =====")
+		fmt.Println("   ===== ===== ===== ===== ===== ===== ===== ===== =====   ")
+	}
 }
--- main.go
+++ main.go
@@ -4,132 +4,14 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
 
 	"github.com/tkivisik/go-play/games"
 )
-
-// BoardSideLength determines the height and width of the game board
-const BoardSideLength uint8 = 8
-
-// Letters hold the alphabet
-const Letters string = "abcdefghijklmnopqrstuvwxyz"
-
-// MaxShips determiens the number of ships per player
-const MaxShips int = 2
-
-// MaxShots determines the maximum length of a game
-const MaxShots int = 100
-
-// Legend is a dictionary of program logic to map elements
-var Legend = map[string]string{
-	"terrain": "~",
-	"ship":    "0",
-	"hit":     "X",
-	"miss":    "*",
-}
-
-// PrintLegend prints the legend on screen
-func PrintLegend() {
-	fmt.Println("")
-	for k, v := range Legend {
-		fmt.Printf("%2s - %s    ", v, k)
-	}
-	fmt.Println("\n Valid point example: 'd 3'")
-	fmt.Print("\n")
-}
-
-// PrintBoard prints the current battleship board
-func PrintBoard(ships, shots map[int8]byte) int {
-	hitCount := 0
-	fmt.Print("\n  ")
-	for i := uint8(0); i < BoardSideLength; i++ {
-		fmt.Printf("%2s", Letters[i:i+1])
-	}
-	fmt.Println()
-
-	for i := uint8(0); i < BoardSideLength; i++ {
-		fmt.Printf("%2d", i+1)
-		for j := uint8(0); j < BoardSideLength; j++ {
-			if ships[int8(i)]&(1<<j) != 0 {
-				if shots[int8(i)]&(1<<j) != 0 {
-					fmt.Printf(" %s", Legend["hit"])
-					hitCount++
-				} else {
-					fmt.Printf(" %s", Legend["ship"])
-				}
-			} else {
-				if shots[int8(i)]&(1<<j) != 0 {
-					fmt.Printf(" %s", Legend["miss"])
-				} else {
-					fmt.Printf(" %s", Legend["terrain"])
-				}
-			}
-		}
-		fmt.Println()
-	}
-	fmt.Println()
-	return hitCount
-}
-
-// Shoot updates the shots map
-func PointToMap(point Point, landscape map[int8]byte) {
-	point.number--
-	var letterIndex uint8 = uint8(strings.Index(Letters, point.letter))
-	landscape[int8(point.number)] |= 1 << uint8(letterIndex)
-}
-
-type Point struct {
-	letter string
-	number int
-}
-
-// PlaceShips guides the player to place its ships
-func PlaceShips() map[int8]byte {
-	var point Point
-	ships := map[int8]byte{0: uint8(0)}
-	shots := map[int8]byte{}
-
-	fmt.Printf("Please choose the location of your %d ships (e.g. 'd 3')\n", MaxShips)
-	for i := 0; i < MaxShips; i++ {
-		PrintBoard(ships, shots)
-		fmt.Scanln(&point.letter, &point.number)
-		PointToMap(point, ships)
-	}
-	PrintBoard(ships, shots)
-	return ships
-}
-
-func PlaceShots(ships map[int8]byte) {
-	var point Point
-	var Scoreboard = map[string]int{
-		"me":    0,
-		"enemy": 0,
-	}
-	shots := map[int8]byte{}
-
-	fmt.Print("Please choose where to shoot (e.g. 'd 2')\n\n")
-	for i := 0; i < MaxShots; i++ {
-		fmt.Scanln(&point.letter, &point.number)
-		PointToMap(point, shots)
-		PrintLegend()
-		Scoreboard["me"] = PrintBoard(ships, shots)
-		if Scoreboard["me"] >= MaxShips {
-			fmt.Println(" ALL SHIPS SUNK! WELL DONE!")
-			break
-		}
-	}
-}
 
 func main() {
 	var n = flag.Int("n", 1, "rounds to play")
 	var game = flag.String("game", "", "game to play (e.g. 'num', 'bin')")
 	flag.Parse()
-
-	fmt.Println(" BATTLESHIP THE GAME\n")
-
-	ships := PlaceShips()
-	PlaceShots(ships)
 
 	for i := 0; i < *n; i++ {
 		switch *game {
