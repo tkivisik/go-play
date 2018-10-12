adc08d549d9c25283b21e590e9032813a523de9f
Taavi Kivisik <taavi.kivisik@gmail.com>
2018-10-12 11:41:40 +0200

    Layer

--- coordinate/landscape.go
+++ coordinate/landscape.go
@@ -11,6 +11,29 @@ const (
 	boardSideLength uint8  = 4
 )
 
+// Legend holds the mapping of a board
+type Legend struct {
+	Terrain rune
+	Ship    byte
+	Hit     byte
+	Miss    byte
+}
+
+var legend = Legend{
+	rune("~"),
+	[]byte("0")[0],
+	[]byte("X")[0],
+	[]byte("*")[0],
+}
+
+/* legend is a dictionary of program logic to map elements
+var legend = map[string]string{
+	"terrain": "~",
+	"ship":    "0",
+	"hit":     "X",
+	"miss":    "*",
+}*/
+
 // Coordinate expresses a location on a map as a
 type Coordinate struct {
 	x byte
@@ -49,14 +72,46 @@ func (c *Coordinate) Read() {
 	}
 }
 
-type Board struct {
+type layer struct {
+	layer [boardSideLength]byte
+}
+
+func (l *layer) StringRaw() string {
+	var str strings.Builder
+
+	for row := uint8(0); row < boardSideLength; row++ {
+		str.WriteString(fmt.Sprintf("%08b\n", l.layer[row]))
+	}
+	return str.String()
+}
+
+func (l *layer) coordinateToOne(c Coordinate) {
+	l.layer[c.y] |= 1 << c.x
+}
+
+type board struct {
+	ships layer
+	shots layer
 }
 
 func CoordinatePart() {
-	/*var c Coordinate = Coordinate{2, 2}
-	c.Read()
+	var ll = layer{[boardSideLength]byte{4, 8, 16, 32}}
+	fmt.Println(ll)
+	fmt.Printf("%T\n", ll)
+	fmt.Printf("%08b\n", ll.layer)
+	fmt.Printf("%s\n", ll.StringRaw())
+
+	var c = Coordinate{5, 2}
+	//c.Read()
 	fmt.Printf("Internal representation: %+v\n", c)
-	fmt.Println(c.String())*/
+	fmt.Println(c.String())
 	//	var board = Board{}
+	ll.coordinateToOne(c)
+	fmt.Println(ll.StringRaw())
+
+	fmt.Printf("%s", legend.Terrain)
+	fmt.Print("%s", legend.Hit)
+	fmt.Print("%s", legend.Miss)
+	fmt.Print("%s", legend.Ship)
 
 }
