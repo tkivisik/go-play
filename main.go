312d7d019bff339ca104dbfcec5e8b39c559d18d
Taavi Kivisik <taavi.kivisik@gmail.com>
2018-10-20 16:13:27 +0200

    clean up commented out code

--- main.go
+++ main.go
@@ -27,21 +27,4 @@ import (
 
 func main() {
 	cmd.Execute()
-	/*	var n = flag.Int("n", 1, "rounds to play")
-		var game = flag.String("game", "", "game to play (e.g. 'num', 'bin')")
-		flag.Parse()
-
-		for i := 0; i < *n; i++ {
-			switch *game {
-			case "num":
-				games.NumberGuessing()
-			case "bin":
-				games.BinaryGuessing()
-			case "ship":
-				games.Battleship()
-			default:
-				fmt.Println("Pass a flag -game with either 'num' or 'bin' or 'ship'")
-				games.Battleship()
-			}
-		}*/
 }
